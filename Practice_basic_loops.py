afec59353db22850bf2b93cfe1c70669e6ecf646
prakkshiwalia <164944476+prakkshiwalia@users.noreply.github.com>
2025-02-06 12:00:08 -0500

    Basic python program
    This is the program

---
+++ Practice_basic_loops.py
@@ -0,0 +1,25 @@
+#Write a program to display your name 108 times
+
+count = 1
+
+while count <= 108:
+	print(count, " Prakkshi")
+	count = count + 1
+
+#WAP to display your name 500 times along with serial number
+
+serial_number = 1
+
+while serial_number <= 500:
+	print("Serial_number is" , serial_number, " Harshit")
+	serial_number = serial_number + 1 
+
+# Natural numbers - Which are positive whole numbers and does not include fractions. Traditionally starts with 0 in some contexts
+
+Natural_number = 1
+
+while Natural_number <= 50:
+	print(Natural_number)
+	Natural_number = Natural_number + 1
+
+
